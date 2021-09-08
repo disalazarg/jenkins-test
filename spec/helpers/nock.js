7531027b9bfb4b73a125b98941d0561b3d1997a9
Daniel Salazar <disalazarg@gmail.com>
2021-09-08 16:34:07 -0300

    node-config integration

--- spec/helpers/nock.js
+++ spec/helpers/nock.js
@@ -1,5 +1,6 @@
 const nock = require( 'nock' )
+const cfg  = require( 'config' )
 
-nock( 'https://dsalazar.cl' )
-  .post( '/shipping/api/events/jenkins/job' )
+nock( cfg.base_url )
+  .post( cfg.path )
   .reply( 200, ( url, req ) => req )
